c9e7f8ffa357f554cb7c0255dd85adb9d951679a
sandeepam5588 <78194280+sandeepam5588@users.noreply.github.com>
2021-06-05 13:29:06 +0530

    react app with team page

--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -1,5 +1,6 @@
 package io.javabrains.ipldashboard.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import io.javabrains.ipldashboard.repository.MatchRepository;
 import io.javabrains.ipldashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin //it allows webpages from other domains to make a call to this api
 public class TeamController {
 
     private TeamRepository teamRepository;
